8d6e6850d2bcb54418b517419dc84fb41a591ffd
lucianolopess <luckferrlopes@gmail.com>
2019-11-26 16:16:38 -0300

    created some interceptors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-
-
+import { DirectivesModule } from './directives/directives.module';
+import { ErroServidorInterceptor } from './interceptors/erro-servidor-interceptor';
+import { JwtInterceptor } from './interceptors/jwt-interceptor';
+import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    PipesModule,
+    DirectivesModule
+ ],
+ exports: [
+    PipesModule,
+    DirectivesModule
+ ],
+ providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor , multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: ErroServidorInterceptor , multi: true},
+  ] 
 })
 export class SharedModule { }
