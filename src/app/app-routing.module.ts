a45bcce699d3a6becab776f04973c7805dfa70f9
lucianolopess <luckferrlopes@gmail.com>
2019-11-28 15:19:34 -0300

    added supermercado module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ErrosComponent } from './erros/erros/erros.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pedidos', pathMatch: 'full' },
+  { path: 'supermercados', loadChildren: () => import(`./supermercados/supermercados.module`).then(m => m.SupermercadosModule) },
   { path: 'pedidos', loadChildren: () => import(`./pedido/pedido.module`).then(m => m.PedidoModule) },
   { path: 'admin', loadChildren: () => import(`./admin/admin.module`).then(m => m.AdminModule) },
   { path: 'login', loadChildren: () => import(`./login/login.module`).then(m => m.LoginModule) },
