85f101e04a5871d5c65cc2560bd9b646ac7650cd
lucianolopess <luckferrlopes@gmail.com>
2019-11-29 10:51:09 -0300

    added docker file

--- src/app/admin/admin-routing/admin-routing.module.ts
+++ src/app/admin/admin-routing/admin-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'supermercados', loadChildren: () => import(`../supermercado/supermercado.module`).then(m => m.SupermercadoModule) },
+  // { path: 'supermercados', loadChildren: () => import(`../supermercado/supermercado.module`).then(m => m.SupermercadoModule) },
+  { path: 'supermercados', loadChildren: '../supermercado/supermercado.module#SupermercadoModule' }
 ];
 
 @NgModule({
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { ErrosComponent } from './erros/erros/erros.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pedidos', pathMatch: 'full' },
-  { path: 'supermercados', loadChildren: () => import(`./supermercados/supermercados.module`).then(m => m.SupermercadosModule) },
-  { path: 'pedidos', loadChildren: () => import(`./pedido/pedido.module`).then(m => m.PedidoModule) },
-  { path: 'admin', loadChildren: () => import(`./admin/admin.module`).then(m => m.AdminModule) },
-  { path: 'login', loadChildren: () => import(`./login/login.module`).then(m => m.LoginModule) },
+  // { path: 'supermercados', loadChildren: () => import(`./supermercados/supermercados.module`).then(m => m.SupermercadosModule) },
+  // { path: 'pedidos', loadChildren: () => import(`./pedido/pedido.module`).then(m => m.PedidoModule) },
+  // { path: 'admin', loadChildren: () => import(`./admin/admin.module`).then(m => m.AdminModule) },
+  // { path: 'login', loadChildren: () => import(`./login/login.module`).then(m => m.LoginModule) },
+  { path: 'supermercados', loadChildren: './supermercados/supermercados.module#SupermercadosModule' },
+  { path: 'pedidos', loadChildren: './pedido/pedido.module#PedidoModule' },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginModule' },
   { path: 'error', component: ErrosComponent },
   { path: '**', component: ErrosComponent, data: { error: 404 } },
 ];
